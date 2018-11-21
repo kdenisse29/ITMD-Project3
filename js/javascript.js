fa153ccb1bfe28ccb493d0a277de68e93e34200c
Kelly <kdenisse29@gmail.com>
2018-11-20 21:44:34 -0600

    map marker

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,6 @@
 function initMap(){
-	var uluru = {lat: 19.7060, lng: -101.1950};
+	var uluru = {lat: 19.7060, lng: 101.1950};
 	var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 10,center: uluru});
+	 var marker = new google.maps.Marker({position: uluru, map: map});
 }
